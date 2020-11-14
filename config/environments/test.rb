dc59b0f47aee61c40bb9e0cb00a563ed3205eac8
manhcuongdtbk <manhcuongdtbk@gmail.com>
2020-11-14 22:42:38 +0700

    Add Bullet

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -48,4 +48,11 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+  # Bullet
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.rails_logger = true
+    Bullet.raise = true
+  end
 end
